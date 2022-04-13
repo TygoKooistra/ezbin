060b1747666bcf039608964b06215bfc5a8d83ba
TYcommand <TYcommand@protonmail.com>
2022-04-13 13:11:40 +0200

    Added parse_file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+}
+
+fn parse_file(path: String) {
+  let code = fs::read_to_string(path)
+    .expect("Could not load a file");
+  
+  for c in code.chars() {
+    
+  }
 }
