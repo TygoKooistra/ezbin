c5dbe26f00c823e3c9c0f9f5d802571b8847ed41
TYcommand <TYcommand@protonmail.com>
2022-04-13 13:45:01 +0200

    Added example.ezbin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::env;
 use std::fs;
 
+
+fn print_usage() {
+  print!("EZbin usage:
+<filename>    Load a file into the processing queue
+-o <filename> Set output file
+");
+}
+
 fn main() {
-  println!("Hello, world!");
+  let mut args: Vec<String> = env::args().collect();
+  args.remove(0);
+
+
+
+
+  let mut lastArg = String::from("");
+  for arg in args {
+
+  }
+
+
+
 }
 
 fn parse_file(path: String) {
@@ -9,6 +30,6 @@ fn parse_file(path: String) {
     .expect("Could not load a file");
   
   for c in code.chars() {
-    
+
   }
 }
